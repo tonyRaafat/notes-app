23b4afb1b0d1f163c3d62f1652282f5dd2173e84
tonyRaafat <138820743+tonyRaafat@users.noreply.github.com>
2023-11-14 20:38:33 +0200

    create databaseUser

---
+++ lib/views/services/crud/notes_services.dart
@@ -0,0 +1,24 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart' show join;
+import 'package:path_provider/path_provider.dart'
+    show getApplicationDocumentsDirectory;
+
+class DatabaseUser {
+  final int id;
+  final String email;
+
+  DatabaseUser({required this.id, required this.email});
+
+  DatabaseUser.fromRow(Map<String,Object?> map): id = map[idColumn] as int , email = map[emailColumn] as String;
+
+  @override
+  String toString() => "Person ID = $id, email = $email";
+
+  @override
+  bool operator == (covariant DatabaseUser other)=> id == other.id;
+
+  @override
+  int get hashCode => id.hashCode;
+}
+const idColumn = "id";
+const emailColumn = "email";
